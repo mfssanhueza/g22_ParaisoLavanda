cbd7d2de98f2ab5ccbc70fe7c64239f7b9262a19
Fernanda Salas <mfssanhueza@gmail.com>
2019-07-05 16:51:52 -0400

    login with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  devise :omniauthable, :omniauth_providers => [:facebook]
   has_many :likes
   has_many :stories, :through => :likes
   has_many :shopping_carts
   has_many :products, :through => :shopping_carts
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'home#index'
-  devise_for :users
   post 'likes/:id/create', to: 'likes#create', as: 'like'
   resources :stories, only: [:index, :show]
   resources :products, only: [:index, :show]
   resources :shopping_carts, only: [:create, :destroy]
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
