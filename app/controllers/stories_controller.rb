41a740e353985aa4d0d6a8477a5842f8bf3d2190
Fernanda Salas <mfssanhueza@gmail.com>
2019-07-14 22:10:49 -0400

    minor improvement

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -7,5 +7,7 @@ class StoriesController < ApplicationController
   def show
     @story = Story.find(params[:id])
     @user_like = @story.likes.where(user_id: current_user.id)
+    @story_first_half = @story.content[0..(@story.content.length/2)]
+    @story_second_half = @story.content[(@story.content.length/2)..(@story.content.length)]
   end
 end
