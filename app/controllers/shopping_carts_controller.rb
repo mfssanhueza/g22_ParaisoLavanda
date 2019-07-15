f00786604bcae1d0e6026f07c90db5c6ce0f100c
Fernanda Salas <mfssanhueza@gmail.com>
2019-07-15 12:20:43 -0400

    improved shopping cart

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -7,12 +7,20 @@ class ShoppingCartsController < ApplicationController
     @shopping_cart.user = current_user
     @shopping_cart.product = Product.find(params[:product_id])
     @shopping_cart.save
-    redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      format.json { head :no_content }
+      format.js { }
+    end
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    shopping_cart = ShoppingCart.find(params[:id])
-    shopping_cart.destroy
-    redirect_back(fallback_location: root_path)
+    @shopping_cart = ShoppingCart.find(params[:id])
+    @shopping_cart.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+      format.js { }
+    end
+    # redirect_back(fallback_location: root_path)
   end
 end
